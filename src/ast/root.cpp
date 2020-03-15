ff596d0acbb0256a78f11aa229bebdcd003d35ca
Manuel Liebchen <hallo@manuelliebchen.de>
2020-03-15 20:49:18 +0100

    removing reordering functions

--- src/ast/root.cpp
+++ src/ast/root.cpp
@@ -36,10 +36,6 @@ ASTRoot::ASTRoot(std::string file, const std::shared_ptr<peg::Ast> &ast)
 void ASTRoot::compile(std::ostream &out) {
   std::string last_op = "";
   try {
-    // Calculating compiling order
-    //    last_op = "Generate compiling Order";
-    //    reorder_functions();
-
     last_op = "Determiining return types";
     auto build_context = std::make_unique<by::bc::BuildContext>(file);
     for (const auto &func : externs) {
@@ -82,68 +78,6 @@ void ASTRoot::get_dependencies(std::unordered_set<std::string> &functions,
   for (const auto &func : this->functions) {
     func->get_dependencies(functions, types);
   }
-}
-
-void ASTRoot::reorder_functions() {
-
-  std::vector<std::string> order;
-  std::unordered_map<std::string, std::unordered_set<std::string>> functions;
-  for (const auto &func : this->functions) {
-    std::unordered_set<std::string> all_dep;
-    std::unordered_set<std::string> type_dep;
-    func->get_dependencies(all_dep, type_dep);
-    functions.emplace(func->get_name(), all_dep);
-  }
-
-  std::unordered_map<std::string, std::unordered_set<std::string>> list;
-  for (auto &func : functions) {
-    if (func.first == "main") {
-      list.insert(functions.extract(func.first));
-      break;
-    }
-  }
-  if (list.empty()) {
-    throw std::runtime_error("No main function found!");
-  }
-
-  while (!list.empty()) {
-    auto current = *list.begin();
-    list.erase(current.first);
-    functions.erase(current.first);
-    order.push_back(current.first);
-    for (const std::string &dep : current.second) {
-      if (dep == current.first) {
-        continue;
-      }
-      bool add = true;
-      for (auto func : functions) {
-        if (dep == func.first) {
-          continue;
-        }
-        add &= func.second.find(dep) == func.second.end();
-      }
-      if (add) {
-        if (auto it = functions.find(dep); it != functions.end()) {
-          list.insert(*it);
-        }
-      }
-    }
-  }
-  std::reverse(order.begin(), order.end());
-
-  std::vector<std::shared_ptr<by::ast::ASTFunction>> new_functions;
-  for (const std::string &func_name : order) {
-    ;
-    auto func =
-        std::find_if(this->functions.begin(), this->functions.end(),
-                     [&](std::shared_ptr<by::ast::ASTFunction> const &f) {
-                       return f->get_name() == func_name;
-                     });
-    if (func != this->functions.end()) {
-      new_functions.push_back(*func);
-    }
-  }
-  this->functions = new_functions;
 }
 
 auto operator<<(std::ostream &os, const ASTRoot &root) -> std::ostream & {
